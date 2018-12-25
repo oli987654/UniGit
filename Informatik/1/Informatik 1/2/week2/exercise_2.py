cd8ed4d82631f7c5783f7bee6652370ca56d700a
Oliver Hliddal <ohlidd@Olivers-Air.fritz.box>
2018-12-26 00:02:19 +0100

    Removed ds_store and .doc

--- Informatik/1/Informatik 1/2/week2/exercise_2.py
+++
@@ -1,4 +0,0 @@
-name = input('What is your name?')
-age = input('How old are you?')
-occupation = input('What is your occupation?')
-print('Hi %s! I see you are %s years old and work as a %s.' % (name, age, occupation))
